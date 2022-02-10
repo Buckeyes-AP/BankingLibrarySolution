fd7c1fba2f8bbdbaf31b9a04f2c5631923e2d77c
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-02-10 12:21:22 -0500

    New Testing Unit

--- AccountFormatLibrary/FormatAccount.cs
+++ AccountFormatLibrary/FormatAccount.cs
@@ -6,6 +6,9 @@ namespace AccountFormatLibrary {
         public static decimal 
             CalculateInterestByMonths(int months, decimal interestRate, decimal balance) 
         {
+            if(balance < 0) {
+                throw new ArgumentException("Balance cannot be negative!");
+            }
             return balance * (interestRate / 12) * months;
 
         } 
